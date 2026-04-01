d3e216bcc820b32e845d03d4656491383c83ad19
marvelandcat-ux <marvelandcat@gmail.com>
2026-04-01 15:19:37 +0900

    텍스트 프로그램 잘가라~

--- Assets/C Study/NumberGame.cs
+++ Assets/C Study/NumberGame.cs
@@ -4,7 +4,11 @@ using UnityEngine.InputSystem;
 
 public class NumberGame : MonoBehaviour
 {
-    int tryCount = 0;
+    private int answer = 3;
+    private const int Maxtry = 5;
+    private int tryCount = 0;
+    private bool isOver = false;
+
     void Start()
     {
         Debug.Log("--------------------");
@@ -12,21 +16,22 @@ public class NumberGame : MonoBehaviour
         Debug.Log("1~5사이의 숫자를 맞춰보세요");
         Debug.Log("[1].[2],[3].[4],[5]키를 누르세요");
         Debug.Log("--------------------");
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
         int input = -1;
+        if (isOver) return;
 
-        if(Input.GetKeyDown(KeyCode.Alpha1)) input =1;
-        if(Input.GetKeyDown(KeyCode.Alpha2)) input =2;
-        if(Input.GetKeyDown(KeyCode.Alpha3)) input =3;
-        if(Input.GetKeyDown(KeyCode.Alpha4)) input =4;
-        if(Input.GetKeyDown(KeyCode.Alpha5)) input =5;
+        if (Input.GetKeyDown(KeyCode.Alpha1)) input = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) input = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) input = 3;
+        if (Input.GetKeyDown(KeyCode.Alpha4)) input = 4;
+        if (Input.GetKeyDown(KeyCode.Alpha5)) input = 5;
 
-        if(input != -1)
+        if (input != -1)
         {
             CheckAnswer(input);
         }
@@ -34,15 +39,29 @@ public class NumberGame : MonoBehaviour
 
     private void CheckAnswer(int input)
     {
-       
+
         tryCount++;
         Debug.Log($"입력: {input}, 시도횟수: {tryCount}");
-        if(tryCount > 5)
+        if (input == answer)
         {
-            Debug.Log("실패!");
-            
+            Debug.Log($"정답입니다!{tryCount}번 만에맞췄습니다.");
+            isOver=true;
+            return;
         }
 
-
+        if (tryCount >= Maxtry)
+        {
+            Debug.Log($"실패 정답은{answer}이였습니다! ");
+            isOver=true;
+            return;
+        }
+        if (input > answer)
+        {
+            Debug.Log("더 작은 숫자입력해주세요");
+        }
+        else
+        {
+            Debug.Log("더 큰 숫자를 입력해주세요");
+        }
     }
 }
